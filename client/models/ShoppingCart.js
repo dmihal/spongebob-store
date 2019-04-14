b293ae6f469a4347573193bf8ad354ba252b7143
David Mihal <david@dmihal.com>
2019-04-14 21:43:25 +0200

    Persist shopping cart

--- client/models/ShoppingCart.js
+++ client/models/ShoppingCart.js
@@ -1,5 +1,23 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 const ShoppingCart = new Mongo.Collection(null);
+
+Meteor.startup(function loadStoredCart() {
+  const storedCart = localStorage.getItem('cart');
+  if (storedCart) {
+    JSON.parse(storedCart).forEach(item => ShoppingCart.insert(item));
+  }
+});
+
+const saveCart = () => setTimeout(
+  () => localStorage.setItem('cart', JSON.stringify(ShoppingCart.find().fetch())), 10
+);
+
+ShoppingCart.find().observe({
+  added: saveCart,
+  changed: saveCart,
+  removed: saveCart,
+});
 
 export default ShoppingCart;
